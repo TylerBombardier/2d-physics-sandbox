11b3bb301e4536da4bcd5f2c7ef8d203cf8b8146
Tyler Bombardier <W0870190@myscc.ca>
2025-11-17 14:02:45 -0500

    Removed PIXI dependency, refocused project to be purely Matter.JS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-import * as PIXI from 'pixi.js';
 import Matter from 'matter-js';
 
 //Destructuring to extract specific modules
-const { Engine, Render, Runner, Bodies, World } = Matter;
-const { Application } = PIXI;
+const { Engine, Render, Runner, Bodies, World, Composite } = Matter;
 
 let engine = Engine.create(); //Engine creation
 let world = engine.world; //World creation
@@ -11,17 +9,31 @@ let world = engine.world; //World creation
 /**
  * Creating the PIXI canvas
  */
-let container = document.getElementById("sim-area");
-let bounds = container.getBoundingClientRect();
+let sceneContainer = document.getElementById("sim-area");
+let bounds = sceneContainer.getBoundingClientRect();
 
-const app = new Application();
-await app.init({
+let render = Render.create({
+  element: sceneContainer,
+  engine: engine,
+  options: {
     width: bounds.width,
     height: bounds.height,
-    backgroundColor: new PIXI.Color('white'),
-    antialias: true,
-    resolution: 1,
-    preference: 'webgl'
-})
+    wireframes: false,
+    background: 'transparent',
+  }
+});
 
-container.appendChild(app.canvas);
+// create two boxes and a ground
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(window.innerWidth/2, window.innerHeight+25, window.innerWidth, 50, { isStatic: true });
+
+// add all of the bodies to the world
+Composite.add(engine.world, [boxA, boxB, ground]);
+
+Render.run(render);
+Render.setPixelRatio(render, window.devicePixelRatio); 
+
+let runner = Runner.create();
+
+Runner.run(runner, engine);
